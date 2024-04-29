6f86ff3bba9ba2743c68cee8d3f549957164e283
Sahil Kotian <sahil.kotian316@gmail.com>
2024-04-28 20:41:40 -0400

    link updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom';  // Import Link component
 
 function Navbar() {
 
   function logout() {
-    localStorage.removeItem('currentUser')
-    window.location.href='/login'
+    localStorage.removeItem('currentUser');
+    window.location.href = '/login'; // Consider replacing this with history.push if using React Router's useHistory for SPA behavior
   }
 
   return (
     <div>
-      <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg">
+        <Link className="navbar-brand" to="/">
           GRUBZEN
-        </a>
+        </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarNav"
@@ -22,41 +23,34 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"><i className='fa fa-bars' style={{color: 'white'}}></i></span>
+          <span className="navbar-toggler-icon"><i className='fa fa-bars' style={{color: 'white'}}></i></span>
         </button>
-        <div class="collapse navbar-collapse " id="navbarNav">
-
-         
-
-
-          <ul class="navbar-nav ml-auto">
-
-          {localStorage.getItem('currentUser') ? (
-            <div class="dropdown mr-5">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-user" aria-hidden="true"></i>  {JSON.parse(localStorage.getItem('currentUser')).name} 
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="/profile">Profile</a>
-              <a class="dropdown-item" href="#" onClick={logout}>Logout</a>
-            </div>
-          </div>
-
-          ) : (
-            <>
-            <li class="nav-item active">
-              <a class="nav-link" href="/register">
-                Register
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/login">
-                Login
-              </a>
-            </li>
-            </>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            {localStorage.getItem('currentUser') ? (
+              <div className="dropdown mr-5">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i className="fa fa-user" aria-hidden="true"></i> {JSON.parse(localStorage.getItem('currentUser')).name}
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <Link className="dropdown-item" to="/profile">Profile</Link>
+                  <button className="dropdown-item" onClick={logout}>Logout</button>
+                </div>
+              </div>
+            ) : (
+              <>
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </>
             )}
-           
           </ul>
         </div>
       </nav>
